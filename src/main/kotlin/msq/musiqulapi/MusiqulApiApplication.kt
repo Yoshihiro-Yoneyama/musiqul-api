05fc63760104c035c1c3c86088573667fd893db4
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-01 21:31:34 +0900

    first commit

---
+++ src/main/kotlin/msq/musiqulapi/MusiqulApiApplication.kt
@@ -0,0 +1,11 @@
+package msq.musiqulapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusiqulApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusiqulApiApplication>(*args)
+}
---
+++ src/test/kotlin/msq/musiqulapi/MusiqulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package msq.musiqulapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusiqulApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
