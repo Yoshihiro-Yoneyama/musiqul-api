5c897e19c4d3f20c657ad3ac6dd3c584c35c09fb
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-25 00:32:09 +0900

    GetWeatherControllerを作成する

---
+++ src/main/kotlin/msq/musiqulapi/presentation/weather/GetWeatherController.kt
@@ -0,0 +1,22 @@
+package msq.musiqulapi.presentation.weather
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GetWeatherController {
+  @GetMapping("/weather")
+  fun getPerson(): GetWeatherResponse {
+    return GetWeatherResponse(
+      weatherId = "123",
+      label = "label",
+      title = "title",
+    )
+  }
+}
+
+data class GetWeatherResponse(
+  val weatherId: String,
+  val label: String,
+  val title: String,
+)
