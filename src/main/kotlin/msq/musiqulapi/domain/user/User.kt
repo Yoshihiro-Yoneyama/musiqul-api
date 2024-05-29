55904804902aa27d9e66310a2967dc2d2e401e0b
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-05-29 11:04:58 +0900

    ユーザードメイン再作成

--- src/main/kotlin/msq/musiqulapi/domain/user/User.kt
+++ src/main/kotlin/msq/musiqulapi/domain/user/User.kt
@@ -1,18 +1,51 @@
 package msq.musiqulapi.domain.user
 
 import lombok.Value
+import java.util.*
 
 @Value(staticConstructor = "of")
 data class User(
   val userId: UserId,
-  val userName: UserName
+  val displayName: DisplayName,
+  val mailAddress: MailAddress,
 ) {
   companion object {
-    fun create(userName: UserName): User {
+    fun create(displayName: DisplayName): User {
       return User(
         userId = UserId.generate(),
-        userName = userName
+        displayName = displayName,
+        mailAddress = MailAddress(""),
       )
     }
+  }
+}
+
+@JvmInline
+value class MailAddress(val value: String) {
+  companion object {
+    private val MAIL_ADDRESS_MAX_LENGTH = 255
+  }
+  init {
+    require(value.isNotBlank()) { "Mail address is required" }
+    require(value.length <= MAIL_ADDRESS_MAX_LENGTH) {
+      "Mail address must be ${MAIL_ADDRESS_MAX_LENGTH} characters or less"
+    }
+  }
+}
+
+@JvmInline
+@Value(staticConstructor = "of")
+value class UserId(val value: String) {
+  companion object {
+    fun generate(): UserId {
+      return UserId(UUID.randomUUID().toString())
+    }
+  }
+}
+
+@JvmInline
+value class DisplayName(val value: String) {
+  init {
+    require(value.length in 1..30) { "User name must be between 1 and 30 characters" }
   }
 }
