e541cd664c2cce4d3b93803f5cab62549628a730
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-06-11 21:55:51 +0900

    GetTaskControllerを作成する

---
+++ src/main/kotlin/msq/musiqulapi/application/query/task/get/GetTaskController.kt
@@ -0,0 +1,12 @@
+package msq.musiqulapi.application.query.task.get
+
+class GetTaskController {
+
+}
+
+data class GetTaskResponse(
+    val id: String,
+    val title: String,
+    val description: String,
+    val due: String,
+)
