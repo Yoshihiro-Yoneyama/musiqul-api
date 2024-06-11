36b7ecc6f04cfb919523827b9bff30972d166f78
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-06-11 11:11:09 +0900

    Userクラスを修正する

--- src/main/kotlin/msq/musiqulapi/domain/user/User.kt
+++ src/main/kotlin/msq/musiqulapi/domain/user/User.kt
@@ -1,7 +1,6 @@
 package msq.musiqulapi.domain.user
 
 import lombok.Value
-import java.util.*
 
 @Value(staticConstructor = "of")
 data class User(
@@ -20,32 +19,3 @@ data class User(
   }
 }
 
-@JvmInline
-value class MailAddress(val value: String) {
-  companion object {
-    private val MAIL_ADDRESS_MAX_LENGTH = 255
-  }
-  init {
-    require(value.isNotBlank()) { "Mail address is required" }
-    require(value.length <= MAIL_ADDRESS_MAX_LENGTH) {
-      "Mail address must be ${MAIL_ADDRESS_MAX_LENGTH} characters or less"
-    }
-  }
-}
-
-@JvmInline
-@Value(staticConstructor = "of")
-value class UserId(val value: String) {
-  companion object {
-    fun generate(): UserId {
-      return UserId(UUID.randomUUID().toString())
-    }
-  }
-}
-
-@JvmInline
-value class DisplayName(val value: String) {
-  init {
-    require(value.length in 1..30) { "User name must be between 1 and 30 characters" }
-  }
-}
