ec9746300832e685f91365aa641e0ce52a51ff4b
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-06-16 17:59:56 +0900

    RecruitmentのValueObjectsクラスを実装する

---
+++ src/main/kotlin/msq/musiqulapi/domain/model/collab/recruitment/ValueObjects.kt
@@ -0,0 +1,75 @@
+package msq.musiqulapi.domain.model.collab.recruitment
+
+import arrow.core.Either
+import msq.musiqulapi.lib.NonEmptyMap
+import msq.musiqulapi.lib.NonEmptyString
+import java.time.Instant
+import java.util.*
+
+@JvmInline
+value class RecruitmentId(val value: UUID) {
+  companion object {
+    fun reconstruct(id: UUID): RecruitmentId {
+      return RecruitmentId(id)
+    }
+  }
+}
+
+@JvmInline
+value class RecruitmentName(val value: String)
+
+enum class MusicGenre {
+  ROCK,
+  J_POP,
+  ANIME,
+  JAZZ,
+  CLASSIC,
+  METAL,
+  OTHER
+}
+
+@JvmInline
+value class SongTitle(val value: NonEmptyString)
+
+enum class Instrument {
+  VOCAL,
+  GITTER,
+  ELECTRIC_BASE,
+  DRUM,
+  KEY_BOARD,
+  PIANO,
+  VIOLIN,
+  OTHER
+}
+
+@JvmInline
+value class RequiredInstrumentsAndCounts(val value: NonEmptyMap<Instrument, Int>)
+
+@JvmInline
+value class RequiredAgeRange private constructor(val value: IntRange) {
+  companion object {
+    fun create(value: IntRange): Either<IllegalArgumentException, RequiredAgeRange> {
+      return if (value.first < 0 || value.last > 120)
+        Either.Left(IllegalArgumentException("age is out of range"))
+      else
+        Either.Right(RequiredAgeRange(value))
+    }
+  }
+}
+
+enum class RequiredGender {
+  MALE_ONLY,
+  FEMALE_ONLY,
+  ALL
+}
+
+@JvmInline
+value class DeadLine(val value: Instant)
+
+@JvmInline
+value class Memo(val value: String)
+
+enum class RecruitmentStatus {
+  OPEN,
+  CLOSE,
+}
