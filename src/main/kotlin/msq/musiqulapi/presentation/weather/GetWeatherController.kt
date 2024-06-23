2ce65af3a5dc9679cc7f1fb5c106602559f0918d
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-06-23 21:50:35 +0900

    ID自動生成実験用

--- src/main/kotlin/msq/musiqulapi/presentation/weather/GetWeatherController.kt
+++ src/main/kotlin/msq/musiqulapi/presentation/weather/GetWeatherController.kt
@@ -1,14 +1,22 @@
 package msq.musiqulapi.presentation.weather
 
+import msq.musiqulapi.domain.model.test_task.TaskId
+import msq.musiqulapi.lib.IdFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.locks.Lock
 
 @RestController
-class GetWeatherController {
+class GetWeatherController(val factory: IdFactory<TaskId>) {
+
   @GetMapping("api/weather")
   fun get(): GetWeatherResponse {
+    val id = factory.generate()
+
+    koukai(::print)
+
     return GetWeatherResponse(
-      weatherId = "123",
+      weatherId = id.value.toString(),
       label = "label",
       title = "title",
     )
@@ -20,3 +28,21 @@ data class GetWeatherResponse(
   val label: String,
   val title: String,
 )
+
+fun koukai(print: () -> String) {
+  print()
+}
+
+fun print(): String {
+ return "aaa";
+}
+
+fun koukai2(kannsuu: (str: String) -> Int): Int {
+  val v = "a"
+  return kannsuu(v)
+}
+
+fun kannsuu(str: String): String {
+  if (str == "a") return "aaa"
+  else return "bbb"
+}
