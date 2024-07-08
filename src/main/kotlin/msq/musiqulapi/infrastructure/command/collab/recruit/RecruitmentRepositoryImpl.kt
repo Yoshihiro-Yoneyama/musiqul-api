c1b05d126fcb7a1012768a85191366b9d58a57ff
yoshihiro <yoshihiro>
2024-07-08 14:31:10 +0900

    RecruitmentRepositoryImpl#saveを実装する

--- src/main/kotlin/msq/musiqulapi/infrastructure/command/collab/recruit/RecruitmentRepositoryImpl.kt
+++ src/main/kotlin/msq/musiqulapi/infrastructure/command/collab/recruit/RecruitmentRepositoryImpl.kt
@@ -1,46 +1,35 @@
 package msq.musiqulapi.infrastructure.command.collab.recruit
 
 import arrow.core.Option
-import msq.musiqulapi.domain.model.collab.recruitment.*
-import msq.musiqulapi.infrastructure.command.collab.recruit.mapper.RecruitmentMapper
-import msq.musiqulapi.infrastructure.command.collab.recruit.mapper.RecruitmentRecord
-import nu.studer.sample.enums.GenderType
-import nu.studer.sample.enums.RecruitmentStatusType
+import msq.musiqulapi.domain.model.collab.recruitment.Recruitment
+import msq.musiqulapi.domain.model.collab.recruitment.RecruitmentId
+import msq.musiqulapi.domain.model.collab.recruitment.RecruitmentRepository
+import msq.musiqulapi.infrastructure.command.collab.recruit.mapper.*
 import org.springframework.stereotype.Repository
-import java.time.LocalDateTime
-import java.time.ZoneId
 
 @Repository
 class RecruitmentRepositoryImpl(
-  private val recruitmentMapper: RecruitmentMapper
+  private val recruitmentTranslator: RecruitmentTranslator,
+  private val recruitmentMapper: RecruitmentMapper,
+  private val recruitmentMusicGenreMapper: RecruitmentMusicGenreMapper,
+  private val recruitmentOwnerInstrumentMapper: RecruitmentOwnerInstrumentMapper,
+  private val recruitmentRecruitedInstrumentMapper: RecruitmentRecruitedInstrumentMapper,
+  private val recruitmentRequiredGenerationMapper: RecruitmentRequiredGenerationMapper
 ) : RecruitmentRepository {
   override fun save(recruitment: Recruitment) {
-    recruitmentMapper.upsert(toRecruitmentRecord(recruitment))
+    recruitmentMapper.upsert(recruitmentTranslator.toRecruitmentRecord(recruitment))
+    recruitmentMusicGenreMapper.delete(recruitment.id.value)
+    recruitmentMusicGenreMapper.insert(recruitmentTranslator.toRecruitmentMusicGenreRecord(recruitment))
+    recruitmentOwnerInstrumentMapper.delete(recruitment.id.value)
+    recruitmentOwnerInstrumentMapper.insert(recruitmentTranslator.toRecruitmentOwnerInstrumentRecord(recruitment))
+    recruitmentRecruitedInstrumentMapper.delete(recruitment.id.value)
+    recruitmentRecruitedInstrumentMapper.insert(recruitmentTranslator.toRecruitmentRecruitedInstrumentRecord(recruitment))
+    recruitmentRequiredGenerationMapper.delete(recruitment.id.value)
+    recruitmentRequiredGenerationMapper.insert(recruitmentTranslator.toRecruitmentRequiredGenerationRecord(recruitment))
   }
 
   override fun findById(recruitmentId: RecruitmentId): Option<Recruitment> {
     TODO("Not yet implemented")
   }
 
-  private fun toRecruitmentRecord(recruitment: Recruitment): RecruitmentRecord {
-    return RecruitmentRecord(
-      recruitment.id.value,
-      recruitment.name.value,
-      recruitment.owner.value,
-      recruitment.songTitle.value,
-      recruitment.artist.value,
-      when (recruitment.requiredGender) {
-        RequiredGender.MALE_ONLY -> GenderType.MALE_ONLY
-        RequiredGender.FEMALE_ONLY -> GenderType.FEMALE_ONLY
-        RequiredGender.ALL -> GenderType.ALL
-      },
-      LocalDateTime.ofInstant(recruitment.deadline.value, ZoneId.of("UTC")),
-      recruitment.memo.value,
-      when (recruitment.recruitmentStatus) {
-        RecruitmentStatus.OPEN -> RecruitmentStatusType.OPEN
-        RecruitmentStatus.CLOSE -> RecruitmentStatusType.CLOSE
-      },
-      recruitment.deleted
-    )
-  }
 }
