d946f9400ead2229444979c87207f659edad3986
yoshihiro <yoshihiro>
2024-07-08 14:27:35 +0900

    プロパティのprivateを削除する

--- src/main/kotlin/msq/musiqulapi/lib/NonEmptyMap.kt
+++ src/main/kotlin/msq/musiqulapi/lib/NonEmptyMap.kt
@@ -1,6 +1,6 @@
 package msq.musiqulapi.lib
 
-data class NonEmptyMap<K, V>(private val map: Map<K, V>) {
+data class NonEmptyMap<K, V>(val map: Map<K, V>) {
   init {
     if (map.isEmpty()) throw IllegalArgumentException("map is empty")
   }
