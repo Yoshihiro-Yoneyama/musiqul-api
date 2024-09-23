5b0245a64d4d6d288ab5ed5cf4b77c73affd4d9c
yoshihiro <yoshihiro>
2024-09-23 19:11:05 +0900

    CollabForSearchQueryRepositoryを作成する

---
+++ src/main/kotlin/msq/musiqulapi/application/query/collab_for_search/CollabForSearchQueryRepository.kt
@@ -0,0 +1,20 @@
+package msq.musiqulapi.application.query.collab_for_search
+
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CollabForSearchQueryRepository {
+
+  /**
+   * 登録されたコラボのリードモデルを追加するメソッド
+   */
+  fun saveRecruitment(recruitment: Recruitment) {}
+
+  fun deleteRecruitment(recruitment: Recruitment) {}
+
+  // なんのメソッドか分かり次第再実装？
+//  fun putRecruitmentOwner(recruitmentOwner: RecruitmentOwner) {}
+
+  // なんのメソッドか分かり次第再実装
+//  fun deleteRecruitmentOwner(recruitmentOwner: RecruitmentOwner) {}
+}
