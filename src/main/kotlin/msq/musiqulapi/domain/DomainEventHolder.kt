88bb2d7939051e2806906f4e85aed5b88734e0d6
yoshihiro <yoneyonekun1@gmail.com>
2025-02-26 20:35:03 +0900

    Feat ( DomainEventHolder.kt ) Create DomainEventHolder.

---
+++ src/main/kotlin/msq/musiqulapi/domain/DomainEventHolder.kt
@@ -0,0 +1,24 @@
+package msq.musiqulapi.domain
+
+
+/**
+ * ドメインイベントを保持する基本クラス
+ *
+ * @param <A> 継承するサブクラスの型
+ */
+@Suppress("UNCHECKED_CAST")
+abstract class DomainEventHolder<A: DomainEventHolder<A>> {
+
+  /** ドメインイベントリスト */
+  private val domainEvents: MutableList<DomainEvent> = mutableListOf()
+
+  protected fun addEvent(event: DomainEvent): A {
+    return this as A
+  }
+
+  fun clearEvents() {
+    domainEvents.clear()
+  }
+
+  fun getEvents(): List<DomainEvent> = domainEvents.toList()
+}
